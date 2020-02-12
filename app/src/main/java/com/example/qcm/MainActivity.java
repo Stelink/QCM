2865067e42354e11fa2e0d741167f12984d17e14
Steven <link_the_boss_@outlook.fr>
2020-02-12 21:07:26 +0100

    aled

--- app/src/main/java/com/example/qcm/MainActivity.java
+++ app/src/main/java/com/example/qcm/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private String id;
-
+    private String mdp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
