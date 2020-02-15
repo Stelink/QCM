b5b9cb0e6cf57b16732fff6b6e52e3482c7c19c9
Steven <link_the_boss_@outlook.fr>
2020-02-15 21:34:28 +0100

    question 3 + xml

--- app/src/main/java/com/example/qcm/TroisiemeQuestionActivity.java
+++ app/src/main/java/com/example/qcm/TroisiemeQuestionActivity.java
@@ -1,6 +1,7 @@
 package com.example.qcm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,11 +12,22 @@ public class TroisiemeQuestionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_troisieme_question);
-
+        this.bool = false;
     }
 
     public void onClickValider(View vue){
+        Intent intent = new Intent(TroisiemeQuestionActivity.this, QuatriemeQuestionActivity.class);
+        if(this.bool)
+            startActivity(intent);
+    }
 
+
+    public void onClickFaux(View vue){
+        this.bool = true;
+    }
+
+    public void onClickVrai(View vue){
+        this.bool = false;
     }
 
     public void onClickAnnuler(View vue){
